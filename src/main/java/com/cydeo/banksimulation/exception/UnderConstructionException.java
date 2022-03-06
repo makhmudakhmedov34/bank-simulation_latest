f0124c5e8771e89d7b0f05d04d9f7edc94bcc048
fatma1990-jd <fatma@cybertekschool.com>
2022-03-06 18:18:13 +0000

    "Bank simulation lab -01 / Spring Boot Steps"

--- src/main/java/com/cydeo/banksimulation/exception/UnderConstructionException.java
+++ src/main/java/com/cydeo/banksimulation/exception/UnderConstructionException.java
@@ -2,6 +2,7 @@ package com.cydeo.banksimulation.exception;
 
 public class UnderConstructionException extends RuntimeException {
     public UnderConstructionException(String s) {
+
         super(s);
     }
 }
