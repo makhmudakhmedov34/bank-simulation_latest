e4d9de155ae77b4a538691d8be6fcb38c7ca4bfd
fatma1990-jd <fatma@cybertekschool.com>
2022-03-06 18:08:39 +0000

    "Bank simulation lab -01 Vasig Spring Boot Steps"

--- src/main/java/com/cydeo/banksimulation/repository/AccountRepository.java
+++ src/main/java/com/cydeo/banksimulation/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package com.cydeo.banksimulation.repository;
 
 import com.cydeo.banksimulation.entity.Account;
+import com.cydeo.banksimulation.exception.RecordNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class AccountRepository {
@@ -17,4 +19,12 @@ public class AccountRepository {
 
     }
 
+    public List<Account> findAll() {
+        return accountList;
+    }
+
+    public Account findById(UUID accountId) {
+       return accountList.stream().filter(account -> account.getId().equals(accountId)).findAny().orElseThrow(() ->
+                new RecordNotFoundException("This account is not in the databse"));
+    }
 }
